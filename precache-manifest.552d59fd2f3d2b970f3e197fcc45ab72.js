edab38f538794940a17a25d1e47c6148bf7d1518
Mahavir Munot <mahavir1408@gmail.com>
2020-10-04 17:54:04 +0530

    Updates

---
+++ precache-manifest.552d59fd2f3d2b970f3e197fcc45ab72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9493b7528f265cdfe1ccc4255553bd",
+    "url": "/al/index.html"
+  },
+  {
+    "revision": "176ac53889b3ed67cd75",
+    "url": "/al/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6d8e4dd9596cba654474",
+    "url": "/al/static/js/2.d68a4139.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/al/static/js/2.d68a4139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "176ac53889b3ed67cd75",
+    "url": "/al/static/js/main.0f87139d.chunk.js"
+  },
+  {
+    "revision": "ff2e772b393187cb1cc8",
+    "url": "/al/static/js/runtime-main.77d6e710.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/al/static/media/logo.5d5d9eef.svg"
+  }
+]);
